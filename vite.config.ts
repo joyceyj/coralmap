e483fbd8a53cc009bcc552a7ffb9c55aa63aacb7
joyceyj <joyce3.yang@gmail.com>
2024-04-15 03:02:37 +0800

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig( ( {mode} : ConfigEnv): UserConfig => {
     return {
+        base: '/coralscop/',
         resolve: {
             alias: {
                 "@": pathSrc,
